912dce94e369ab86bf3dc18047187ac8e043b04f
cyace84 <e1@MacBook-Pro-Zavizion.local>
2021-12-08 04:47:37 +0200

    fixed test

--- test/storage/staking.js
+++ test/storage/staking.js
@@ -12,23 +12,34 @@ module.exports = {
   total_underlying_f: 0,
   last_update_time: "2021-01-01T00:00:00Z",
 
-  // metadata: MichelsonMap.fromLiteral({
-  //   "": Buffer.from("tezos-storage:meta", "ascii").toString("hex"),
-  //   meta: Buffer.from(
-  //     JSON.stringify({
-  //       version: "v0.0.1",
-  //       name: "Staking-bridge",
-  //       description: "Staking contract for bridge",
-  //       authors: ["Madfish.Solutions"],
-  //       homepage: "https://www.madfish.solutions//",
-  //       source: {
-  //         tools: ["Ligo", "Flextesa"],
-  //         location: "https://ligolang.org/",
-  //       },
-  //       interfaces: ["TZIP-012"],
-  //       errors: [],
-  //     }),
-  //     "ascii",
-  //   ).toString("hex"),
-  // }),
+  metadata: MichelsonMap.fromLiteral({
+    "": Buffer.from("tezos-storage:meta", "ascii").toString("hex"),
+    meta: Buffer.from(
+      JSON.stringify({
+        version: "v0.0.1",
+        name: "Staking-bridge",
+        description: "Staking contract for bridge",
+        authors: ["Madfish.Solutions"],
+        homepage: "https://www.madfish.solutions//",
+        source: {
+          tools: ["Ligo", "Flextesa"],
+          location: "https://ligolang.org/",
+        },
+        interfaces: ["TZIP-012"],
+        errors: [],
+      }),
+      "ascii"
+    ).toString("hex"),
+  }),
+  token_metadata: MichelsonMap.fromLiteral({
+    0: {
+      token_id: "0",
+      token_info: MichelsonMap.fromLiteral({
+        symbol: Buffer.from("swABR").toString("hex"),
+        name: Buffer.from("Shares wABR").toString("hex"),
+        decimals: Buffer.from("6").toString("hex"),
+        icon: Buffer.from("").toString("hex"),
+      }),
+    },
+  }),
 };
