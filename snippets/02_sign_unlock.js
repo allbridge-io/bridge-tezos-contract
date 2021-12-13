4cb8ed4785c48d01bfbbe929d657221002a44a2c
cyace84 <e1@MacBook-Pro-Zavizion.local>
2021-12-13 12:52:33 +0200

    worked sign unlock

--- snippets/02_sign_unlock.js
+++ snippets/02_sign_unlock.js
@@ -10,51 +10,62 @@ Tezos.setSignerProvider(signer);
 
 const { Parser, packDataBytes } = require("@taquito/michel-codec");
 const keccak256 = require("keccak256");
+const params = {
+  assetType: "fa12",
+  lockId: 1,
+  recipient: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+  chainFromId: 3536,
+  amount: 1000000,
+  tokenAddress: "KT1RQcMbTJC71y2CDw2r24pp6dergDZXhc35",
+};
 
-const parser = new Parser();
-const type = `(pair (nat %lock_id)
+async function sign() {
+  const parser = new Parser();
+  const type = `(pair (nat %lock_id)
           (pair (address %recipient)
                 (pair (nat %amount)
                       (pair (bytes %chain_from_id)
                             (or %asset
                                (or (address %fa12_) (pair %fa2_ (address %address) (nat %id)))
                                (or (unit %tez_) (pair %wrapped_ (bytes %chain_id) (bytes %native_token_address))))))))`;
-let data;
-switch (params.assetType) {
-  case "fa12":
-    data = `(Pair ${params.lockId}
+  let data;
+  switch (params.assetType) {
+    case "fa12":
+      data = `(Pair ${params.lockId}
       "${params.recipient}"
       ${params.amount}
       0x${params.chainFromId}
       (Left (Left "${params.tokenAddress}")))`;
-    break;
-  case "fa2":
-    data = `(Pair ${params.lockId}
+      break;
+    case "fa2":
+      data = `(Pair ${params.lockId}
       "${params.recipient}"
       ${params.amount}
       0x${params.chainFromId}
       (Left (Right (Pair "${params.tokenAddress}" ${params.tokenId}))))`;
-    break;
-  case "tez":
-    data = `(Pair ${params.lockId}
+      break;
+    case "tez":
+      data = `(Pair ${params.lockId}
       "${params.recipient}"
       ${params.amount}
       0x${params.chainFromId}
       (Right (Left Unit)))`;
-    break;
-  case "wrapped":
-    data = `(Pair ${params.lockId}
+      break;
+    case "wrapped":
+      data = `(Pair ${params.lockId}
       "${params.recipient}"
       ${params.amount}
       0x${params.chainFromId}
       (Right (Right (Pair 0x${params.chainId} 0x${params.tokenAddress}))))`;
-    break;
+      break;
+  }
+  const dataJSON = parser.parseMichelineExpression(data);
+  const typeJSON = parser.parseMichelineExpression(type);
+
+  const packed = packDataBytes(dataJSON, typeJSON);
+  const hashBytes = keccak256(Buffer.from(packed.bytes, "hex")).toString("hex");
+  const signature = await signer.sign(hashBytes);
+  console.log(signature.sig);
 }
-const dataJSON = parser.parseMichelineExpression(data);
-const typeJSON = parser.parseMichelineExpression(type);
 
-const packed = packDataBytes(dataJSON, typeJSON);
-const hashBytes = keccak256(Buffer.from(packed.bytes, "hex")).toString("hex");
-const signature = signer.sign(hashBytes);
-
-console.log(signature.sig);
+sign();
