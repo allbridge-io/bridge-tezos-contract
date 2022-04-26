d64bc3d174352be4050b8bae58e3c2260c0b8753
cyace84 <cyace84@gmail.com>
2022-04-26 19:18:31 +0300

    fixed tests

--- test/04_validator_validate.js
+++ test/04_validator_validate.js
@@ -10,10 +10,12 @@ const Validator = require("./helpers/validatorWrapper");
 const { alice, bob } = require("../scripts/sandbox/accounts");
 const toBytes = require("../scripts/toBytesForSign");
 
-describe("BridgeValidator Validate tests", async function () {
+describe("BridgeValidator Validate tests", async function() {
   let validator;
 
-  const bscChainId = Buffer.from("56", "ascii").toString("hex");
+  const bscChainId = "11223344";
+  const recipient =
+    "1122334455667788990011223344556677889900112233445566778899001122";
   const tezosChainId = Buffer.from("NetXdQprcVkpaWU", "ascii").toString("hex");
   const tokenAddress = alice.pkh;
   before(async () => {
@@ -25,8 +27,8 @@ describe("BridgeValidator Validate tests", async function () {
     }
   });
 
-  describe("Testing entrypoint: Validate_lock", async function () {
-    it("Shouldn't validate if sender is not bridge-contract", async function () {
+  describe("Testing entrypoint: Validate_lock", async function() {
+    it("Shouldn't validate if sender is not bridge-contract", async function() {
       Tezos.setSignerProvider(signerBob);
       await rejects(
         validator.validateLock(
@@ -44,7 +46,7 @@ describe("BridgeValidator Validate tests", async function () {
         },
       );
     });
-    it("Shouldn't validate if the destination chain id is tezos chain id", async function () {
+    it("Shouldn't validate if the destination chain id is tezos chain id", async function() {
       Tezos.setSignerProvider(signerAlice);
       await rejects(
         validator.validateLock(
@@ -65,14 +67,32 @@ describe("BridgeValidator Validate tests", async function () {
         },
       );
     });
-    it("Should validate lock fa12 asset", async function () {
+    it("Shouldn't validate if the recipient wrong recipient", async function() {
+      Tezos.setSignerProvider(signerAlice);
+      await rejects(
+        validator.validateLock(
+          "01ffffffffffffffffffffffffffff00",
+          alice.pkh,
+          10000,
+          Buffer.from(alice.pkh, "ascii").toString("hex"),
+          bscChainId,
+          "fa12",
+          tokenAddress,
+        ),
+        err => {
+          strictEqual(err.message, "Validator-bridge/wrong-recipient");
+          return true;
+        },
+      );
+    });
+    it("Should validate lock fa12 asset", async function() {
       const lockAmount = 10000;
 
       await validator.validateLock(
         "01ffffffffffffffffffffffffffff00",
         bob.pkh,
         lockAmount,
-        Buffer.from(alice.pkh, "ascii").toString("hex"),
+        recipient,
         bscChainId,
         "fa12",
         tokenAddress,
@@ -83,14 +103,14 @@ describe("BridgeValidator Validate tests", async function () {
       );
       notStrictEqual(newLock, undefined);
     });
-    it("Should validate lock fa2 asset", async function () {
+    it("Should validate lock fa2 asset", async function() {
       const lockAmount = 10000;
 
       await validator.validateLock(
         "01ffffffffffffffffffffffffffff01",
         bob.pkh,
         lockAmount,
-        Buffer.from(alice.pkh, "ascii").toString("hex"),
+        recipient,
         bscChainId,
         "fa2",
         tokenAddress,
@@ -102,13 +122,13 @@ describe("BridgeValidator Validate tests", async function () {
       );
       notStrictEqual(newLock, undefined);
     });
-    it("Should validate lock tez asset", async function () {
+    it("Should validate lock tez asset", async function() {
       const lockAmount = 10000;
       await validator.validateLock(
         "01ffffffffffffffffffffffffffff02",
         bob.pkh,
         lockAmount,
-        Buffer.from(alice.pkh, "ascii").toString("hex"),
+        recipient,
         bscChainId,
         "tez",
       );
@@ -118,13 +138,13 @@ describe("BridgeValidator Validate tests", async function () {
       );
       notStrictEqual(newLock, undefined);
     });
-    it("Should validate lock wrapped asset", async function () {
+    it("Should validate lock wrapped asset", async function() {
       const lockAmount = 10000;
       await validator.validateLock(
         "01ffffffffffffffffffffffffffff03",
         bob.pkh,
         lockAmount,
-        Buffer.from(alice.pkh, "ascii").toString("hex"),
+        recipient,
         bscChainId,
         "wrapped",
         alice.pkh,
@@ -136,13 +156,13 @@ describe("BridgeValidator Validate tests", async function () {
       );
       notStrictEqual(newLock, undefined);
     });
-    it("Shouldn't validate if lock is validated", async function () {
+    it("Shouldn't validate if lock is validated", async function() {
       await rejects(
         validator.validateLock(
           "01ffffffffffffffffffffffffffff00",
           bob.pkh,
           10000,
-          Buffer.from(alice.pkh, "ascii").toString("hex"),
+          recipient,
           bscChainId,
           "fa12",
           tokenAddress,
@@ -154,8 +174,8 @@ describe("BridgeValidator Validate tests", async function () {
       );
     });
   });
-  describe("Testing entrypoint: Validate_unlock", async function () {
-    it("Shouldn't validate if sender is not bridge-contract", async function () {
+  describe("Testing entrypoint: Validate_unlock", async function() {
+    it("Shouldn't validate if sender is not bridge-contract", async function() {
       Tezos.setSignerProvider(signerBob);
       const signature = await signerAlice.sign(
         Buffer.from("dasdsa", "ascii").toString("hex"),
@@ -176,7 +196,7 @@ describe("BridgeValidator Validate tests", async function () {
         },
       );
     });
-    it("Shouldn't validate if signature is not validated", async function () {
+    it("Shouldn't validate if signature is not validated", async function() {
       Tezos.setSignerProvider(signerAlice);
       const signature = await signerAlice.sign(
         Buffer.from("dasdsa", "ascii").toString("hex"),
@@ -197,7 +217,7 @@ describe("BridgeValidator Validate tests", async function () {
         },
       );
     });
-    it("Should validate unlock fa12 asset", async function () {
+    it("Should validate unlock fa12 asset", async function() {
       Tezos.setSignerProvider(signerAlice);
       const unlockAmount = 10000;
       const keccakBytes = toBytes({
@@ -226,7 +246,7 @@ describe("BridgeValidator Validate tests", async function () {
       );
       notStrictEqual(newUnlock, undefined);
     });
-    it("Should validate unlock fa2 asset", async function () {
+    it("Should validate unlock fa2 asset", async function() {
       const unlockAmount = 10000;
       const keccakBytes = await toBytes({
         lockId: "01ffffffffffffffffffffffffffff01",
@@ -256,7 +276,7 @@ describe("BridgeValidator Validate tests", async function () {
       );
       notStrictEqual(newUnlock, undefined);
     });
-    it("Should validate unlock tez asset", async function () {
+    it("Should validate unlock tez asset", async function() {
       const unlockAmount = 10000;
       const keccakBytes = await toBytes({
         lockId: "01ffffffffffffffffffffffffffff02",
@@ -281,7 +301,7 @@ describe("BridgeValidator Validate tests", async function () {
       );
       notStrictEqual(newUnlock, undefined);
     });
-    it("Should validate unlock wrapped asset", async function () {
+    it("Should validate unlock wrapped asset", async function() {
       const unlockAmount = 10000;
       const keccakBytes = await toBytes({
         lockId: "01ffffffffffffffffffffffffffff03",
@@ -311,7 +331,7 @@ describe("BridgeValidator Validate tests", async function () {
       );
       notStrictEqual(newUnlock, undefined);
     });
-    it("Shouldn't validate if unlock is validated", async function () {
+    it("Shouldn't validate if unlock is validated", async function() {
       const signature = await signerSecp.sign(
         Buffer.from("dasdsa", "ascii").toString("hex"),
       );
