072a4a2d264af43b9656bf48a0bebc4c17fe3c45
cyace84 <cyace84@gmail.com>
2022-05-11 06:07:26 +0300

    updated bytesToSign script

--- scripts/toBytesForSign.js
+++ scripts/toBytesForSign.js
@@ -2,44 +2,26 @@ const { Parser, packDataBytes } = require("@taquito/michel-codec");
 const keccak256 = require("keccak256");
 function paramToBytes(params) {
   const parser = new Parser();
-  const type = `(pair (bytes %lock_id)
-          (pair (address %recipient)
-                (pair (nat %amount)
-                      (pair (bytes %chain_from_id)
-                            (or %asset
-                               (or (address %fa12_) (pair %fa2_ (address %address) (nat %id)))
-                               (or (unit %tez_) (pair %wrapped_ (address %address) (nat %id))))))))`;
-  let data;
-  switch (params.assetType) {
-    case "fa12":
-      data = `(Pair 0x${params.lockId}
+  const type = `
+        (pair (bytes %lock_id)
+          (address %recipient)
+          (nat %amount)
+          (bytes %chain_from_id)
+          (bytes %token_source)
+          (bytes %token_source_address)
+          (bytes %blockchain_id)
+          (string %type_operation)
+        )`;
+  let data = `
+    (Pair 0x${params.lockId}
       "${params.recipient}"
       ${params.amount}
       0x${params.chainFromId}
-      (Left (Left "${params.tokenAddress}")))`;
-      break;
-    case "fa2":
-      data = `(Pair 0x${params.lockId}
-      "${params.recipient}"
-      ${params.amount}
-      0x${params.chainFromId}
-      (Left (Right (Pair "${params.tokenAddress}" ${params.tokenId}))))`;
-      break;
-    case "tez":
-      data = `(Pair 0x${params.lockId}
-      "${params.recipient}"
-      ${params.amount}
-      0x${params.chainFromId}
-      (Right (Left Unit)))`;
-      break;
-    case "wrapped":
-      data = `(Pair 0x${params.lockId}
-      "${params.recipient}"
-      ${params.amount}
-      0x${params.chainFromId}
-      (Right (Right (Pair "${params.tokenAddress}" ${params.tokenId}))))`;
-      break;
-  }
+      0x${params.tokenSource}
+      0x${params.tokenSourceAddress}
+      0x${params.blockchainId}
+      "unlock")`;
+
   const dataJSON = parser.parseMichelineExpression(data);
   const typeJSON = parser.parseMichelineExpression(type);
 
